da57fcab89648f29ec560b0fdbdf09bec182ed7f
Jacobus Burger <therealjacoburger@gmail.com>
2024-03-02 15:05:39 -0800

    began work on module and related functions

---
+++ menace.py
@@ -0,0 +1,30 @@
+
+
+def save(file, matchboxes, generation):
+    """
+    save(file, matchboxes, generation)
+
+    Serialize the matchboxes and generation number to enable
+        persistent memory of MENACE.
+    """
+    pass
+
+
+def load(file):
+    """
+    load(file)
+
+    Deserialize a json file and return the matchboxes and generation
+        stored within it. For persistent memory of MENACE.
+    """
+    pass
+
+
+def learn(matchboxes, winner):
+    """
+    learn(matchboxes, winner)
+
+    Backpropogate and reinforce winning/tie beads to help MENACE
+        learn what moves work well for it and what moves do not.
+    """
+    pass
