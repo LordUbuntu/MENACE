579e23dc2d1184bae9a5fa553f39992365c64339
Jacobus Burger <therealjacoburger@gmail.com>
2024-03-04 18:00:05 -0800

    fix name conflicts on windows

--- __main__.py
+++ __main__.py
@@ -1,3 +1,116 @@
+# Jacobus Burger (2024)
+# MENACE (short for Matchbox Educable Noughts and Crosses Engine) was
+#   an early convolutional neural network. This program is a replication
+#   of the basic principle of MENACE in Python 3.
+# see: https://en.wikipedia.org/wiki/Matchbox_Educable_Noughts_and_Crosses_Engine
+# TODO:
+# create a seperate module for MENACE functions (save, load, train, etc)
+# create a python package definition/setup
+# create a __main__ executable, allowing users to either import MENACE itself, or play tic-tac-toe against MENACE based on import or run
+# final tweaks
+from time import sleep
+from os.path import exists
+from os import system, name
+from random import choice
+import json  # for persistent memory
+
+
+DELAY = 0.5  # number of seconds to wait before displaying MENACE's move
+
+
+REWARD = 2
+TIE = 1
+PUNISH = 1
+
+
+NO_ONE = 0
+MENACE = 1
+PLAYER = 2
+CHAR_MAP = {
+    NO_ONE: ' ',
+    MENACE: 'O',
+    PLAYER: 'X',
+}
+open_tiles = [*range(9)]  # tiles that can still be selected from
+board_state = [0] * 9
+actions = []    # A list of tuples to remember which beads were chosen
+                #   from which board state. Ephemeral, lifetime is the
+                #   span of the current game only.
+                # (bead_number, board_state)
+# MENACE memory, the matchboxes.
+#   Chooses a random bead based on board state.
+#   Each bead is a number representing a possible tile, 9 choices are
+#   generated based on what tiles are open, basically just a range of all
+#   open spaces on novel board states
+matchboxes = {
+    "         ": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+}
+# TODO: add a way to record the number of generations the memory has trained through and display that
+# NOTE: interestingly, don't need to remember move order or number at all!
+
+
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+
+def board_string(board_state):
+    return ''.join(CHAR_MAP[n] for n in board_state)
+
+
+def show_board(board_state):
+    board = board_string(board_state)
+    for i in range(2):
+        print('|'.join(board[i * 3 : i * 3 + 3]))
+        print("-+-+-")
+    print('|'.join(board[6:9]))
+
+
+def load_memory():
+    # load memory if it exists
+    if exists("matchboxes.json"):
+        with open("matchboxes.json", "r") as file:
+            matchboxes = json.load(file)
+    # otherwise use default empty head
+    else:
+        matchboxes = { "         ": [0, 1, 2, 3, 4, 5, 6, 7, 8] }
+    return matchboxes
+
+
+def save_memory(matchboxes):
+    with open("matchboxes.json", "w") as file:
+        json.dump(matchboxes, file)
+
+
+def winner(board_state):
+    # I love snake lang 🐍
+    for i in range(3):
+        # check for rows
+        if all(state == MENACE for state in board_state[i * 3 : i * 3 + 3]):
+            return MENACE
+        if all(state == PLAYER for state in board_state[i * 3 : i * 3 + 3]):
+            return PLAYER
+        # check for columns
+        if all(state == MENACE for state in board_state[i :: 3]):
+            return MENACE
+        if all(state == PLAYER for state in board_state[i :: 3]):
+            return PLAYER
+    # check for diagonals
+    #   check top-right to bottom-left
+    if all(state == MENACE for state in board_state[2 : 7 : 2]):
+        return MENACE
+    if all(state == PLAYER for state in board_state[2 : 7 : 2]):
+        return PLAYER
+    #   check top-left to bottom-right
+    if all(state == MENACE for state in board_state[0 :: 4]):
+        return MENACE
+    if all(state == PLAYER for state in board_state[0 :: 4]):
+        return PLAYER
+    return NO_ONE
+
+
 def main():
     # retrieve any memory if it exists
     matchboxes = load_memory()
