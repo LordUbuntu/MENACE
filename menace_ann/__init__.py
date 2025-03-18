2da6675c44f768fe42f5a7fb7f91175fd13fc63c
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-17 23:25:02 -0700

    plan to continue

--- menace_ann/__init__.py
+++ menace_ann/__init__.py
@@ -26,6 +26,7 @@ matchboxes = { "         ": [0, 1, 2, 3, 4, 5, 6, 7, 8] }
 generation = 0
 # interesting note: move order doesn't need to be remembered, only the choice for each board state
 # TODO: make this relative to where script is located instead
+# and then remove filename argument since it isn't needed after
 DATA_PATH=".local/bin/menace_ann"
 
 def save(filename, generation, matchboxes):
