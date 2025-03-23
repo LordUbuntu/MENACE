89276a6edb9ecc0678b4fe5772ec55a7d2bd8678
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-22 20:10:12 -0700

    moved all code to __main__.py

--- menace_ann/__main__.py
+++ menace_ann/__main__.py
@@ -3,17 +3,117 @@
 #   an early convolutional neural network. This program is a replication
 #   of the basic principle of MENACE in Python 3.
 # see: https://en.wikipedia.org/wiki/Matchbox_Educable_Noughts_and_Crosses_Engine
-from .__init__ import *
 from time import sleep
 from random import choice, randint
+from random import choice
+import json
+import os
 
 
+
+
+
+# what square "colors" are open on the current board state
+open_tiles = [*range(9)]
+# constants representing each player occupancy with a number
+NO_ONE = 0
+MENACE = 1
+PLAYER = 2
+# which tile is occupied by which player
+board_state = [NO_ONE] * 9
+# constants representing the number of beads added/removed in learning
+REWARD = 2
+TIE = 1
+PUNISH = 1
+# which bead was picked for which board state represented as a list of tuples of (bead_number, board_state), for learning
+actions = []
+# the "neural network" of MENACE. Represents each board state as a hashable string with a corresponding list reprensenting the matchbox of beads
+matchboxes = { "         ": [0, 1, 2, 3, 4, 5, 6, 7, 8] }
+# the current generation of MENACE
+generation = 0
+# interesting note: move order doesn't need to be remembered, only the choice for each board state
 DELAY = 0.5  # number of seconds to wait before displaying MENACE's move
 CHAR_MAP = {
     NO_ONE: ' ',
     MENACE: 'O',
     PLAYER: 'X',
 }
+if os.name == "nt":
+    # Windows is a mess, so I've chosen to just put data in whatever the
+    # current directory is when the user runs the script. Is this ~robust~?
+    # NO. I'm betting on the fact that the group of people who use Windows
+    # AND install Python3 packages with Pip AND run scripts they installed
+    # with pip in terminal AND would pay attention to this project are so
+    # small that I'll never have to deal with it.
+    # It's "bad practice", but perfection is overrated anyways.
+    DATA_PATH=os.getcwd()
+else:
+    DATA_PATH=f"/home/{os.getlogin()}/.local/bin/matchboxes.json"
+
+
+
+
+
+def save(generation, matchboxes):
+    """
+    save(filename, generation, matchboxes)
+
+    Serialize current [generation, matchboxes] for persistent storage.
+    """
+    with open(DATA_PATH, "w") as file:
+        try:
+            json.dump([generation, matchboxes], file)
+        except:
+            print("failed to save to {}".format(DATA_PATH))
+
+
+def load():
+    """
+    load(filename)
+
+    Deserialize a json file and return [generation, matchboxes]
+        stored within it. For persistent memory of MENACE.
+    If no file exists, return default struct definitions instead.
+    """
+    if os.path.exists(DATA_PATH):
+        with open(DATA_PATH, "r") as file:
+            generation, matchboxes = json.load(file)
+    else:
+        generation = 0
+        matchboxes = { "         ": [0, 1, 2, 3, 4, 5, 6, 7, 8] }
+    return [generation, matchboxes]
+
+
+def learn(winner, matchboxes, actions, open_tiles):
+    """
+    learn(matchboxes, winner, actions)
+
+    Backpropogates learned information into matchboxes based on who
+        the winner is using move informtion from actions.
+    """
+    # punish MENACE by taking away some of the bead colours used from
+    #   their matchboxes during the game
+    if winner == PLAYER:
+        for bead, state in actions:
+            for _ in range(PUNISH):
+                # ensure there's always at least 1 bead in each matchbox
+                if len(matchboxes[state]) <= 1:
+                    matchboxes[state] = choice(open_tiles)
+                else:
+                    matchboxes[state].remove(bead)
+    # reward MENACE by adding back more of the bead colours used from
+    #   their matchboxes during the game
+    if winner == MENACE:
+        for bead, state in actions:
+            for _ in range(REWARD):
+                matchboxes[state].append(bead)
+    # encourage MENACE to explore more by adding more of a random bead
+    #   into each matchbox
+    if winner == NO_ONE:
+        for _, state in actions:
+            for _ in range(TIE):
+                bead = choice(matchboxes[state])
+                matchboxes[state].append(bead)
 
 
 def clear():
@@ -66,7 +166,7 @@ def winning_player(board_state):
 
 def main():
     # retrieve any memory if it exists
-    generation, matchboxes = load("matchboxes.json")
+    generation, matchboxes = load()
     # start the game
     game_running = True
     while game_running:
@@ -165,7 +265,7 @@ def main():
             break
 
     # store any learned info from the game
-    save("matchboxes.json", generation + 1, matchboxes)
+    save(generation + 1, matchboxes)
 
 
 if __name__ == '__main__':
